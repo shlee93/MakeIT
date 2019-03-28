63b87ae5721ddaabc4a200e6f4287caac24e88fe
YongJin <YongJin@DESKTOP-MOE51ED>
2019-03-28 09:29:31 +0900

    용진 푸쉬

--- MakeIT/src/main/java/com/kh/makeit/board/model/service/BoardServiceimpl.java
+++ MakeIT/src/main/java/com/kh/makeit/board/model/service/BoardServiceimpl.java
@@ -1,5 +1,6 @@
 package com.kh.makeit.board.model.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.makeit.board.model.dao.BoardDao;
+import com.kh.makeit.board.model.vo.BoardAttach;
+import com.kh.makeit.buy.model.vo.BuyAttach;
+import com.kh.makeit.common.MakeitException;
 
 @Service
 public class BoardServiceimpl implements BoardService {
@@ -66,6 +70,72 @@ public class BoardServiceimpl implements BoardService {
 	public int deleteComment(int commentNo) {
 		// TODO Auto-generated method stub
 		return dao.deleteComment(commentNo);
+	}
+
+
+	@Override
+	public int insertBoardList(ArrayList<BoardAttach> files, Map<String, String> map) {
+		
+		int result =0;
+		
+		try {
+			result = dao.insertBoard(map);
+			System.out.println("보드 인서트 : "+result);
+			System.out.println("map~~ : "+map);
+			System.out.println("map!!!!~~~~~!! : "+map.get("writeContent"));
+			System.out.println("map2~~ : "+Integer.parseInt(map.get("freeNo")));
+			for(BoardAttach ba : files) {
+				System.out.println("실행~~~~~~~~~~~~~~~~~~~~");
+				ba.setFreeNo(Integer.parseInt(map.get("freeNo")));
+				result = dao.insertBoardImg(ba);
+				if(result > 0) {
+					
+				}
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+			
+		return result;
+	}
+	
+	@Override
+	public int updateBoardList(ArrayList<BoardAttach> files, Map<Object, Object> map, int freeNo) {
+		
+		int result =0;
+		
+		try {
+			result = dao.updateBoard(map);
+			System.out.println("보드 인서트 : "+result);
+
+			for(BoardAttach ba : files) {
+				System.out.println("실행~~~~~~~~~~~~~~~~~~~~");
+				/*ba.setFreeNo(Integer.parseInt(map.get("freeNo")));*/
+/*				result = dao.deleteBoardImg(ba, freeNo);
+				result = dao.insertBoardImg(ba, freeNo);*/
+				if(result > 0) {
+					
+				}
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+			
+		return result;
+	}
+
+	@Override
+	public List<Map<String, String>> selectMemberSortBoard(Map<Object,Object> sort, int cPage, int numPerPage) {
+		// TODO Auto-generated method stub
+		return dao.selectMemberSortBoard(sort,cPage,numPerPage);
+	}
+
+	@Override
+	public int selectMemberCountBoard() {
+		// TODO Auto-generated method stub
+		return dao.selectMemberCountBoard();
 	}
 
 }
